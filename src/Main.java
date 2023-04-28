ad6d5130afc394e252f7ccd83d08e8625a115816
Piotr Gorczyński <p.gorczynski4@gmail.com>
2023-04-28 23:18:16 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        readFromAFile("C:/Users/Piotr/Desktop/client-car-purchase-spreadsheet.csv");
+    }
+
+    public static void readFromAFile(String fileName) throws IOException
+    {
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            String everything = sb.toString();
+        }
+    }
+}
