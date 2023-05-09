631fbd7adb11b47341ad90322a8a17ac03f6e695
Piotr Gorczyński <p.gorczynski4@gmail.com>
2023-05-09 22:03:58 +0200

    Fixes in parsing and displaying text on the screen

--- src/main/java/CSVParser.java
+++ src/main/java/CSVParser.java
@@ -4,6 +4,7 @@ import com.opencsv.CSVReaderBuilder;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,6 +12,7 @@ public class CSVParser
 {
     public void parse()
     {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         try
         {
             CSVReader reader = new CSVReaderBuilder(new FileReader("C:\\Users\\Piotr\\Desktop\\project_data.csv")).
@@ -20,19 +22,25 @@ public class CSVParser
         {
             Purchase csvObject = new Purchase();
             csvObject.setId(data[0]);
-            csvObject.person.setName(data[1]);
-            csvObject.person.setSurname(data[2]);
-            csvObject.person.setEmail(data[3]);
-            csvObject.person.setIp(data[4]);
-            csvObject.car.setColor(data[5]);
-            csvObject.car.setVin(data[6]);
-            csvObject.car.setCompany(data[7]);
-            csvObject.car.setModel(data[8]);
-            csvObject.car.setModel_year(data[9]);
-            csvObject.car.setPrice(data[10]);
-            csvObject.localization.setCountry(data[11]);
-            csvObject.localization.setCity(data[12]);
-            csvObject.setDateTime(LocalDateTime.parse(data[13]));
+            csvObject.setPerson(csvObject.new Person() {{
+                setName(data[1]);
+                setSurname(data[2]);
+                setEmail(data[3]);
+                setIp(data[4]);
+            }});
+            csvObject.setCar(csvObject.new Car() {{
+                setColor(data[5]);
+                setVin(data[5]);
+                setCompany(data[6]);
+                setModel(data[8]);
+                setModel_year(data[9]);
+                setPrice(data[10]);
+            }});
+            csvObject.setLocalization(csvObject.new Localization() {{
+                setCountry(data[11]);
+                setCity(data[12]);
+            }});
+//            csvObject.setDateTime(LocalDateTime.parse(data[13], formatter));
             return csvObject;
 
 
--- src/main/java/Purchase.java
+++ src/main/java/Purchase.java
@@ -2,6 +2,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 
+
 public class Purchase
 {
         String id;
@@ -60,5 +61,195 @@ public class Purchase
             this.dateTime = dateTime;
         }
 
+    @Override
+    public String toString()
+    {
+        return "Purchase{" +
+                "id='" + id + '\'' +
+                ", person=" + person +
+                ", car=" + car +
+                ", localization=" + localization +
+                ", dateTime=" + dateTime +
+                '}';
+    }
 
+    public class Person {
+        String name = "";
+        String surname;
+        String email;
+        String ip;
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public String getSurname()
+        {
+            return surname;
+        }
+
+        public String getEmail()
+        {
+            return email;
+        }
+
+        public String getIp()
+        {
+            return ip;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public void setSurname(String surname)
+        {
+            this.surname = surname;
+        }
+
+        public void setEmail(String email)
+        {
+            this.email = email;
+        }
+
+        public void setIp(String ip)
+        {
+            this.ip = ip;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", surname='" + surname + '\'' +
+                    ", email='" + email + '\'' +
+                    ", ip='" + ip + '\'' +
+                    '}';
+        }
+    }
+
+    public class Localization
+    {
+        String country;
+        String city;
+
+        public String getCountry()
+        {
+            return country;
+        }
+
+        public void setCountry(String country)
+        {
+            this.country = country;
+        }
+
+        public String getCity()
+        {
+            return city;
+        }
+
+        public void setCity(String city)
+        {
+            this.city = city;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Localization{" +
+                    "country='" + country + '\'' +
+                    ", city='" + city + '\'' +
+                    '}';
+        }
+    }
+
+    public class Car{
+        String color;
+        String vin;
+        String company;
+        String model;
+        String model_year;
+        BigDecimal price;
+
+        public String getColor()
+        {
+            return color;
+        }
+
+        public String getVin()
+        {
+            return vin;
+        }
+
+        public String getCompany()
+        {
+            return company;
+        }
+
+        public String getModel()
+        {
+            return model;
+        }
+
+        public String getModel_year()
+        {
+            return model_year;
+        }
+
+        public BigDecimal getPrice()
+        {
+            return price;
+        }
+
+        public void setColor(String color)
+        {
+            this.color = color;
+        }
+
+        public void setVin(String vin)
+        {
+            this.vin = vin;
+        }
+
+        public void setCompany(String company)
+        {
+            this.company = company;
+        }
+
+        public void setModel(String model)
+        {
+            this.model = model;
+        }
+
+        public void setModel_year(String model_year)
+        {
+            this.model_year = model_year;
+        }
+
+        public void setPrice(BigDecimal price)
+        {
+            this.price = price;
+        }
+
+        public void setPrice(String datum)
+        {
+            this.price = price;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Car{" +
+                    "color='" + color + '\'' +
+                    ", vin='" + vin + '\'' +
+                    ", company='" + company + '\'' +
+                    ", model='" + model + '\'' +
+                    ", model_year='" + model_year + '\'' +
+                    ", price=" + price +
+                    '}';
+        }
+    }
 }
